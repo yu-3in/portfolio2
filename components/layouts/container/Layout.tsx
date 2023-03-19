5541c8cbef2ff5ef39a217c030e255316af5c500
yuuumiravy <hikoki.y@gmail.com>
2023-03-19 18:03:59 +0900

    Add: Layout components

--- components/layouts/container/Layout.tsx
+++ components/layouts/container/Layout.tsx
@@ -1,5 +1,11 @@
-export type LayoutProps = { children: React.ReactNode }
+import classNames from 'classnames'
 
-export const Layout: React.FC<LayoutProps> = ({ children }) => {
-  return <div className="p-2 px-4">{children}</div>
+export type LayoutProps = { children: React.ReactNode; className?: string }
+
+export const Layout: React.FC<LayoutProps> = ({ children, className }) => {
+  return (
+    <div className={classNames(className, `relative min-h-screen`)}>
+      <main>{children}</main>
+    </div>
+  )
 }
