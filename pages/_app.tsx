c61fa62abd31e3ebf7588ce812721c36bcb84588
yuuumiravy <hikoki.y@gmail.com>
2023-03-19 18:05:15 +0900

    Add: フォントファミリーをBaloo thambi 2に変更

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,23 @@
+import { Layout } from '@/components/layouts/container'
 import '@/styles/globals.css'
+import { NextPage } from 'next'
 import type { AppProps } from 'next/app'
+import { Baloo_Thambi_2 } from 'next/font/google'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+type MyAppProps = AppProps & {
+  Component: NextPage & {
+    getLayout?: (page: React.ReactNode) => React.ReactNode
+  }
+}
+
+const balooThambi2 = Baloo_Thambi_2({ subsets: ['latin'] })
+
+export default function App({ Component, pageProps }: MyAppProps) {
+  const getLayout = Component.getLayout || ((page: React.ReactNode) => page)
+
+  return (
+    <Layout className={balooThambi2.className}>
+      {getLayout(<Component {...pageProps} />)}
+    </Layout>
+  )
 }
