edd281ad6832f550b7db657e61ddbea369a546b5
yuuumiravy <hikoki.y@gmail.com>
2023-03-20 18:03:58 +0900

    Add: Work type

---
+++ features/work/types/Work.ts
@@ -0,0 +1,28 @@
+import { Skill } from '@/features/skill/types/Skill'
+import { Image } from '@/types/Image'
+import { WorkCategory } from './WorkCateogry'
+import { WorkTag } from './WorkTag'
+
+export type Work = {
+  id: string
+  createdAt: string
+  updatedAt: string
+  publishedAt: string
+  revisedAt: string
+  title: string
+  slug: string
+  description: string
+  content?: string
+  categories: WorkCategory[]
+  tags: WorkTag[]
+  role?: string
+  skills?: Skill[]
+  impressions?: string
+  coverImage?: Image
+  thumbnail?: Image
+  startDate: string
+  endDate?: string
+  duration?: string
+  favoriteCount?: number
+  highlight: boolean
+}
