17bca5f8a493ed97baf8df01c6d61f3cdea1ffb9
yuuumiravy <hikoki.y@gmail.com>
2023-03-20 18:02:27 +0900

    Add: ヘッダーに影を追加

--- components/layouts/header/Header.tsx
+++ components/layouts/header/Header.tsx
@@ -6,7 +6,7 @@ export type HeaderProps = {}
 export const Header: React.FC<HeaderProps> = () => {
   return (
     <div className="fixed top-8 left-1/2 z-30 mx-auto w-10/12 -translate-x-1/2">
-      <div className="rounded-[1.4rem] bg-white px-6 py-5">
+      <div className="rounded-[1.4rem] bg-white px-6 py-5 shadow-md">
         <div className="flex items-center justify-between">
           <Link href="/">
             <span className=" bg-gradient-to-b from-black to-[rgba(0,148,255,.5)] bg-clip-text text-2xl font-semibold tracking-wider text-transparent">
