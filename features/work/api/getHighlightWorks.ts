4619373ed0b083fbf9ab00dbd0aa62c3102bb9d5
yuuumiravy <hikoki.y@gmail.com>
2023-03-20 18:04:48 +0900

    Add: work一覧を取得するAPI関数

--- features/work/api/getHighlightWorks.ts
+++ features/work/api/getHighlightWorks.ts
@@ -1,9 +1,9 @@
 import { client } from '@/libs/api/client'
 import { Work } from '../types/Work'
 
-export const getWorks = async (): Promise<Work[] | null> => {
+export const getHighlightWorks = async (): Promise<Work[] | null> => {
   return client
-    .get({ endpoint: 'works', queries: { limit: 20 } })
+    .get({ endpoint: 'works', queries: { filters: 'highlight[equals]true' } })
     .then((res) => res.contents)
     .catch((err) => err)
 }
