8584808bdecfadc24173fce789b57d60d26547d7
yuuumiravy <hikoki.y@gmail.com>
2023-04-24 02:04:28 +0900

    add: Containerコンポーネントに、左右寄せの機能を追加

--- components/layouts/container/Container.tsx
+++ components/layouts/container/Container.tsx
@@ -5,20 +5,28 @@ export type ContainerProps = {
   children: React.ReactNode
   className?: string
   size?: 'medium' | 'large'
+  align?: 'center' | 'left' | 'right'
 }
 
 export const Container: React.FC<ContainerProps> = ({
   children,
   className,
   size = 'medium',
+  align = 'center',
 }) => {
   return (
     <div
-      className={classNames(className, 'relative mx-auto', {
-        'w-11/12 px-4 py-2 md:w-10/12 lg:w-8/12 xl:w-7/12 2xl:w-6/12':
-          size === 'medium',
-        'w-11/12 px-4 py-2 md:w-10/12 lg:w-10/12 xl:w-9/12 2xl:w-9/12':
-          size === 'large',
+      className={classNames(className, 'relative mx-auto px-4 py-2', {
+        'w-11/12 md:w-10/12 lg:w-8/12 xl:w-7/12 2xl:w-6/12':
+          align === 'center' && size === 'medium',
+        'w-11/12 md:w-10/12 lg:w-10/12 xl:w-9/12 2xl:w-9/12':
+          align === 'center' && size === 'large',
+        'ml-0 pl-0': align === 'left',
+        'mr-0 pr-0': align === 'right',
+        'w-[95.83%] md:w-[91.66%] lg:w-[83.33%] xl:w-[79.16%] 2xl:w-[75%]':
+          align !== 'center' && size === 'medium',
+        'w-[95.83%] md:w-[91.66%] lg:w-[91.66%] xl:w-[87.5%] 2xl:w-[87.5%]':
+          align !== 'center' && size === 'large',
       })}
       style={{ minWidth: MIN_WIDTH }}
     >
