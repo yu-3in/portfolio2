e1ec77109dca4a7aa90efcb232b5e105b9c4dc62
yuuumiravy <hikoki.y@gmail.com>
2023-05-27 00:17:44 +0900

    feat: Containerのpxを削除

--- components/layouts/container/Container.tsx
+++ components/layouts/container/Container.tsx
@@ -16,7 +16,7 @@ export const Container: React.FC<ContainerProps> = ({
 }) => {
   return (
     <div
-      className={classNames(className, 'relative mx-auto px-4 py-2', {
+      className={classNames(className, 'relative mx-auto px-0 py-2', {
         'w-11/12 md:w-10/12 lg:w-8/12 xl:w-7/12 2xl:w-6/12':
           align === 'center' && size === 'medium',
         'w-11/12 md:w-10/12 lg:w-10/12 xl:w-9/12 2xl:w-9/12':
