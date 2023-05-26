d04abc149b08b835732bc206ec367dd63536b8af
yuuumiravy <hikoki.y@gmail.com>
2023-05-26 23:24:00 +0900

    fix: WorkCardのwidthを調整

--- features/work/components/WorkList.tsx
+++ features/work/components/WorkList.tsx
@@ -7,14 +7,14 @@ export type WorkListProps = { works: Work[] | null }
 
 export const WorkList: React.FC<WorkListProps> = ({ works }) => {
   return (
-    <ul className="flex list-none flex-wrap justify-center gap-8 after:block after:min-w-[max(45%,310px)] after:max-w-[380px] after:flex-1 after:content-[''] md:after:min-w-[47%]">
+    <ul className="flex list-none flex-wrap justify-center gap-6 after:block after:min-w-[min(100%,400px)] after:max-w-[380px] after:flex-1 after:content-[''] md:after:min-w-[47%] lg:gap-8">
       {works ? (
         works.map((work) => (
           <Fragment key={work.id}>
             <WorkCard
               work={work}
               className={classNames(
-                'min-w-[max(45%,310px)] max-w-[310px] flex-none overflow-hidden rounded-2xl bg-white shadow-md  sm:max-w-[380px] sm:flex-1 md:min-w-[47%]',
+                'min-w-[min(100%,400px)] max-w-[310px] flex-none overflow-hidden rounded-2xl bg-white shadow-md  sm:max-w-[380px] sm:flex-1 md:min-w-[47%]',
               )}
             />
           </Fragment>
