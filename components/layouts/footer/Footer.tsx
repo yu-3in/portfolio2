97b8ec113a9efbb06350dc7346f3d61d5894d8cb
yuuumiravy <hikoki.y@gmail.com>
2023-05-28 15:28:15 +0900

    change: コピーライトの年

--- components/layouts/footer/Footer.tsx
+++ components/layouts/footer/Footer.tsx
@@ -4,7 +4,7 @@ export const Footer: React.FC<FooterProps> = () => {
   return (
     <footer className="mt-12 pb-2">
       <div className="flex justify-center">
-        <small>&copy; 2021 miravy</small>
+        <small>&copy; 2023 miravy</small>
       </div>
     </footer>
   )
