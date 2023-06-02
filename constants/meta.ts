c6aa52b5f80fbe2ed91a16dacaf57d6b30118c3d
yuuumiravy <hikoki.y@gmail.com>
2023-06-02 14:53:27 +0900

    fix: constantsのmetaを修正

--- constants/meta.ts
+++ constants/meta.ts
@@ -1,9 +1,9 @@
 export const SITE_META = {
   siteTitle: "KOKI YUMOTO's PORTFOLIO",
-  siteDesc: 'ポートフォリオサイトです',
-  siteUrl: 'https://example.com',
+  siteDesc:
+    '過去の成果物や技術スタック、ビジョンなどをまとめたポートフォリオサイト',
+  siteUrl: 'https://yuuum.vercel.app/',
   siteLang: 'ja',
   siteLocale: 'ja_JP',
   siteType: 'website',
-  siteIcon: '/favicon.png',
 }
