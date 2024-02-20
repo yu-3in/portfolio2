666399f9a61e83c053cf460af5debb58a36928e1
yu-3in <misharv.y@gmail.com>
2024-02-20 13:22:10 +0900

    feat: heading to bold

--- components/elements/content/parseHTML.tsx
+++ components/elements/content/parseHTML.tsx
@@ -37,6 +37,48 @@ export const replace = (domNode: DOMNode) => {
             {domToReact(domNode.children, { replace })}
           </ul>
         )
+      case 'ol':
+        return (
+          <ol {...domNode.attribs} className="ml-4 list-decimal">
+            {domToReact(domNode.children, { replace })}
+          </ol>
+        )
+      case 'h1':
+        return (
+          <h2 {...domNode.attribs} className="mt-6 mb-4 text-2xl font-bold">
+            {domToReact(domNode.children)}
+          </h2>
+        )
+      case 'h2':
+        return (
+          <h3 {...domNode.attribs} className="mt-4 mb-2 text-xl font-bold">
+            {domToReact(domNode.children)}
+          </h3>
+        )
+      case 'h3':
+        return (
+          <h4 {...domNode.attribs} className="my-2 text-lg font-bold">
+            {domToReact(domNode.children)}
+          </h4>
+        )
+      case 'h4':
+        return (
+          <h5 {...domNode.attribs} className="text-md my-2 font-bold">
+            {domToReact(domNode.children)}
+          </h5>
+        )
+      case 'h5':
+        return (
+          <h6 {...domNode.attribs} className="my-2 text-sm font-bold">
+            {domToReact(domNode.children)}
+          </h6>
+        )
+      case 'h6':
+        return (
+          <p {...domNode.attribs} className="my-2 text-sm font-bold">
+            {domToReact(domNode.children)}
+          </p>
+        )
     }
   }
 }
